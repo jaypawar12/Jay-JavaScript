356b0bfac3185c99c17218e907154048ceb5b8ee
Jay <pawarjay684@gmail.com>
2024-11-20 18:34:10 +0530

    var-decration 20-11-24

---
+++ javascript/day-2.js
@@ -0,0 +1,30 @@
+// initalization
+
+var a;
+// let, const
+let x;
+const PI = 3.14;   // assign
+
+a = 10;
+console.log(a);
+
+/*
+    Data Types :
+    1. primitives =  -number
+                    -string
+                    -boolean
+                    -null
+                    -undefined
+                    -symbol
+                    -bigInt
+    
+    2. non-primitive = -object
+*/
+
+let num = 20;
+console.log(num);
+console.log(typeof num);
+
+let string = "20";
+console.log(string);
+console.log(typeof string);
