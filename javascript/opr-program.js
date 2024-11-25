5a69d3b7a606fae667f8b5b4b5da27f9bda6d03e
Jay <pawarjay684@gmail.com>
2024-11-25 16:45:42 +0530

    Operator Program 25-11-24

---
+++ javascript/opr-program.js
@@ -0,0 +1,74 @@
+// Operators Program
+
+// 1. Convert Doller into Rupess...
+    // let dollar = prompt("Enter How Much Dollar Convert into Rupess.");
+    // let rupee = dollar * 84.28;
+    // console.log( dollar,"Dollar Is Equal To",rupee, "Rupess.");
+
+ 
+// 2. Convert Feet into Inches...
+    // let feet = prompt("Enter How Much Feet Convert into Inches.");
+    // let inches = feet * 12;
+    // console.log( feet,"Feet Is Equal To",inches,"Inches.");
+
+
+// 3. Convert Celsius into Fahrenheit...
+    // let celsius = prompt("Given Temperature in Celsius to Converts into Fahrenheit.");
+    // let fahrenheit = (celsius * 9/5) + 32;
+    // console.log( celsius,"Celsius Is Equal To",fahrenheit,"Fahrenheit.");
+
+
+// 4.  Convert Centimeters into Meters...
+    // let centimeters = prompt("Given Length in Centimeters to Convert into meters.");
+    // let meters = centimeters / 100;
+    // console.log( centimeters,"Centimeters Is Equal To",meters,"Meters.");
+
+
+// 5. Find Out Area Of Triangle...
+    // let base = prompt("Enter Base of Triangle...");
+    // let height = prompt("Enter height of Triangle...");
+    // let triangle = 1/2 * base * height;
+
+    // console.log("The Area of Triangle is :",triangle);
+
+
+// 6. Find Out Area Of Circle...
+    // let pi = 3.14;
+    // let radius = prompt("Enter Radius of Circle...");
+    // let circle = pi * (radius ** 2);
+
+    // console.log("The Area of Circle is :",circle);
+
+
+// 7. Find Out Area Of Rectangle...
+    // let length = prompt("Enter Length of Rectangle...");
+    // let width = prompt("Enter Width of Rectangle...");
+    // let rectangle = length * width;
+
+    // console.log("The Area of Rectangle is :",rectangle);
+
+
+// 8. Find Out Simple Interest...
+    // let principal = prompt("Enter the principal amount :");
+    // let rate = prompt("Enter the rate of interest (in percentage) :");
+    // let time = prompt("Enter the time period (in years) :");
+
+    // let interest = (principal * rate * time) / 100;
+
+    // console.log("Simple Interest :",interest);
+
+
+// 9. Find Out Final Salary Employee (basic, HRA(12%), DA(18%)).
+    // let baseSalary  = prompt("Enter the basic salary of the employee:");
+    // let baseSalary  = 12000;
+    // baseSalary = parseFloat(baseSalary);
+    // let HRA = (baseSalary * 12) / 100;
+    // let DA = (baseSalary * 18) / 100;
+    // let finalSalary = baseSalary + HRA + DA;
+
+    // console.log("The final salary of the employee is :",finalSalary);
+
+
+
+    
+
