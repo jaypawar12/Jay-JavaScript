7f13d0b4f101df59c504ca3571fff490c368f0e1
Jay <pawarjay684@gmail.com>
2024-12-05 14:08:18 +0530

    Update and Add Some New Program 5-12-24

--- javascript/day-7.js
+++ javascript/day-7.js
@@ -120,14 +120,112 @@
 
 // 10. series print out 1, 4, 12, 32, .....
 
-let f = +prompt("Enter a Number :");
-let g = 2;
-let j = 0;
+// let f = +prompt("Enter a Number :");
+// let g = 2, h = 0, a = 1;
 
-for(let i = 1; i <= f; i++){
-    console.log(i * (g ** j));
-    j++;
-}
+//Method : 1
+
+// for(let i = 1; i <= f; i++){
+//     console.log(i * (g ** h));
+//     h++;
+// }
+
+
+//Method : 2
+
+// for (i=1; i<=f; i++){
+//     console.log(a*i);
+//     a*=2;
+// }
 
 // -------------------------------------------------------------------------------------------------
+
+
+// 11. Find Out Factorial of Given Number. 
+
+// let j = +prompt("Enter a Number :");
+// let k = 1;
+
+// for(let i = 1; i <= j; i++){
+//     if (j < 0) {
+//         console.log('Error! Factorial for negative number does not exist.');
+//     }
+//     else if(j === 0){
+//         console.log("The Factorial Number is : 1")
+//     }
+//     else{
+//         k *= i;
+//         console.log(k);
+//     }
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+
+// 12. Enter n and Print Out Total Number's of Sum.
+
+// let l = +prompt("Enter a Number :");
+// let sum = 0;
+
+// for (let i = 1; i <= l; i++) {
+//     if (l > 0) {
+//         sum += i;
+//         console.log("The sum of numbers from 1 to " + i + " is : " + sum);
+//     }
+//     else {
+//         console.log("Error! Please Enter a Positive Number.");
+//     }
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+
+// 13. Enter n and Print Out Total Odd Number's Sum.
+
+// let m = +prompt("Enter a Number :");
+// let oddSum = 0
+
+// for (let i = 1; i <= m; i++) {
+//     if (m > 0) {
+//         if (i % 2 == 1) {
+//             oddSum += i;
+//             console.log("The Sum of Odd Numbers from 1 to " + i + " is : " + oddSum);
+//         }
+//     }
+//     else {
+//         console.log('Error! Please Enter a Positive Number.');
+//     }
+// }
+// -------------------------------------------------------------------------------------------------
+
+
+// 14. Enter n and Print Out Total Even Number's Sum.
+
+// let n = +prompt("Enter a Number :");
+// let evenSum = 0
+
+// for (let i = 1; i <= n; i++) {
+//     if (n > 0) {
+//         if (i % 2 == 0) {
+//             evenSum += i;
+//             console.log("The Sum of Odd Numbers from 1 to " + i + " is : " + evenSum);
+//         }
+//     }
+//     else {
+//         console.log('Error! Please Enter a Positive Number.');
+//     }
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+
+// 15.Enter n and print out sum of digit of a number.(E.g => 741 => 7 + 4 + 1);
+
+let o = +prompt("Enter a Nummber :");   
+let sumDigit = 0;
+
+for (let i = 0; i < o.length; i++) {
+    sumDigit += o[i];
+}   
+console.log("Sum of Digits: " + sumDigit);
 
