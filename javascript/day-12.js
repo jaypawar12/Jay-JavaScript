4df0fde254c9426651ce2e54ed0d6f212f2514fd
Jay <pawarjay684@gmail.com>
2024-12-13 18:53:32 +0530

    Array Method 13-12-24

---
+++ javascript/day-12.js
@@ -0,0 +1,41 @@
+// AARAY
+
+// ARRAY :- Store Multiple Value Single Variable.
+
+// let arr = [11,12,13,14,15]
+// console.log(arr);
+
+// length :- Index Start (0)
+// ARR length :- ShowIn Total length.
+
+// Push ,Pop, shift, upshifts, Splice (ARRAY FUNCTON)
+
+// 1.Push :- New Element Of Add Array
+
+// let data = [11, 12, 13, 14, 15]
+// data.push(55)
+// console.log(data);
+
+// 2.Pop :- Remove At Last Element.
+
+// let data = [11, 12, 13, 14, 15]
+// data.pop()
+// console.log(data);
+
+// 3.Shift :- Remove At First Element.
+
+// let data = [11, 12, 13, 14, 15]
+// data.shift()
+// console.log(data);
+
+// 4. Unshift :- Add To Start.
+
+// let data = [11, 12, 13, 14, 15]
+// data.unshift(10)
+// console.log(data);
+
+// 5. Splice :- Change Original Array (add,remove,replace). [startIdx,delCount,newElement]
+
+// let data = [11, 12, 13, 14, 15]
+// data.splice(1,2,30)
+// console.log(data);
