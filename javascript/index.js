3aa9f1ffcb7edf0c8b436ce03d302b679a5d25da
Pawar Jay <pawarjay684@gmail.com>
2024-12-22 00:17:14 +0530

    Basic Of Calculator

---
+++ javascript/index.js
@@ -0,0 +1,31 @@
+let num1 = +prompt("Enter a Num1");
+let num2 = +prompt("Enter a Num2");
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function reminder() {
+    let result = num1 % num2
+    sumEl.textContent = "Sum: " + result
+}
+
