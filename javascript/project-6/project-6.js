694a9c9a4d5e5738ef1939e0e06f5c86d241a6c9
Pawar Jay <pawarjay684@gmail.com>
2024-12-24 21:34:28 +0530

    Create Slider (Project-6) 24-12-24

--- javascript/project-6/project-6.js
+++ javascript/project-6/project-6.js
@@ -16,18 +16,14 @@ for (let i = 0; i < length; i++) {
 }
 
 const navs = document.querySelectorAll('.nav');
-
-// Set first nav dot as active
 navs[0].classList.add('active');
 
-// Function to reset background color of all navs
 const resetNavs = () => {
     navs.forEach((nav) => {
         nav.classList.remove('active');
     });
 };
 
-// Event listener for nav dot clicks
 navs.forEach((nav, i) => {
     nav.addEventListener("click", () => {
         resetNavs();
@@ -37,43 +33,36 @@ navs.forEach((nav, i) => {
     });
 });
 
-// Change active nav dot based on current slide
 const changeActiveNav = () => {
     resetNavs();
     navs[slideNumber - 1].classList.add('active');
 };
 
-// Move to the next slide
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
     slideNumber++;
 };
 
-// Move to the previous slide
 const prevSlide = () => {
     slideNumber--;
     slider.style.transform = `translateX(-${(slideNumber - 1) * 800}px)`;
 };
 
-// Go to the first slide
 const getFirstSlide = () => {
     slider.style.transform = `translateX(0px)`;
     slideNumber = 1;
 };
 
-// Go to the last slide
 const getLastSlide = () => {
     slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
     slideNumber = length;
 };
 
-// Event listener for the next button
 next.addEventListener("click", () => {
     slideNumber < length ? nextSlide() : getFirstSlide();
     changeActiveNav();
 });
 
-// Event listener for the previous button
 prev.addEventListener("click", () => {
     slideNumber > 1 ? prevSlide() : getLastSlide();
     changeActiveNav();
