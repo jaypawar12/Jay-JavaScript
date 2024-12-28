5262ce3c7cbed4a54861661f05f22e6b6c5bd011
Jay <pawarjay684@gmail.com>
2024-12-28 20:45:10 +0530

    Update project-6.js

--- javascript/project-6/project-6.js
+++ javascript/project-6/project-6.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let slider = document.querySelector(".slider");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
@@ -68,74 +67,3 @@ prev.addEventListener("click", () => {
     slideNumber > 1 ? prevSlide() : getLastSlide();
     changeActiveNav();
 });
-=======
-let slider = document.querySelector(".slider");
-let prev = document.querySelector(".prev");
-let next = document.querySelector(".next");
-
-const images = document.querySelectorAll(".image");
-
-let slideNumber = 1;
-let length = images.length;
-
-const bottom = document.querySelector('.bottom');
-
-for (let i = 0; i < length; i++) {
-    const div = document.createElement('div');
-    div.className = 'nav';
-    bottom.appendChild(div);
-}
-
-const navs = document.querySelectorAll('.nav');
-navs[0].classList.add('active');
-
-const resetNavs = () => {
-    navs.forEach((nav) => {
-        nav.classList.remove('active');
-    });
-};
-
-navs.forEach((nav, i) => {
-    nav.addEventListener("click", () => {
-        resetNavs();
-        slider.style.transform = `translateX(-${i * 800}px)`;
-        slideNumber = i + 1;
-        nav.classList.add('active');
-    });
-});
-
-const changeActiveNav = () => {
-    resetNavs();
-    navs[slideNumber - 1].classList.add('active');
-};
-
-const nextSlide = () => {
-    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-    slideNumber++;
-};
-
-const prevSlide = () => {
-    slideNumber--;
-    slider.style.transform = `translateX(-${(slideNumber - 1) * 800}px)`;
-};
-
-const getFirstSlide = () => {
-    slider.style.transform = `translateX(0px)`;
-    slideNumber = 1;
-};
-
-const getLastSlide = () => {
-    slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
-    slideNumber = length;
-};
-
-next.addEventListener("click", () => {
-    slideNumber < length ? nextSlide() : getFirstSlide();
-    changeActiveNav();
-});
-
-prev.addEventListener("click", () => {
-    slideNumber > 1 ? prevSlide() : getLastSlide();
-    changeActiveNav();
-});
->>>>>>> 42746ee0495050683b7ca481eb9fd38bb6f0ecd0
