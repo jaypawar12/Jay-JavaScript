1f25a1afc58ed415e4bcb0a5892080e5fb5e41a6
Jay <pawarjay684@gmail.com>
2025-01-02 00:54:25 +0530

    Some JS Question

---
+++ javascript/js-question.js
@@ -0,0 +1,79 @@
+// let count = 0;
+// let nums = [0,1,2,3];
+
+// nums.forEach(num =>{
+//     if(num){
+//         count = count + 1;
+//     }
+// })
+
+// console.log(count)
+
+// const arr = [2,3,4,5];
+// // console.log(arr)
+// // console.log(arr.slice(0,1))
+// arr.splice(0,1);
+// console.log(arr)
+// arr.unshift(8);
+// console.log(arr);
+
+
+// const obj = {
+//     a : 1,
+//     b : 2,
+//     c : 3,
+//     "a" : 6,
+//     b : 7
+// }
+
+// console.log(obj) // {a : 1, b : 7, c : 3, "a" : 6}
+
+// console.log(!"Hello");
+// console.log(+false);
+
+// console.log("" == false)
+// console.log("" === false)
+
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+
+// console.log(typeof typeof 1000)
+
+// const arr = [3,4,5,6,7,8]
+// arr[10] = 100;
+// console.log(arr) // []
+
+// function hello(){
+//     xyz = 100;
+//     console.log(xyz)
+// }
+// console.log(hello())
+
+// var xyz = 1 + abc
+// var abc = 100;
+// console.log(xyz)
+
+// const data = {
+//     name : "jay",
+// }
+// const abc = [1,2,3,4]
+// // console.log(delete data)
+// // console.log(delete abc)
+// // console.log(delete abc[1])
+
+// const [x, y] = abc
+// console.log(x, y);
+
+// const a = []
+// const b = []
+
+// console.log(a == b);
+// console.log(a === b);
+
+// function higherOrder(func, value) {
+//     return func(value);
+// }
+
+// const double = num => num * 2;
+
+// console.log(higherOrder(double, 5)); 
