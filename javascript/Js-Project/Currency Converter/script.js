e5377408b18e62f00ea5bcd31c55e5559d36d54a
Jay <pawarjay684@gmail.com>
2025-01-09 15:10:29 +0530

    Currency Converter

---
+++ javascript/Js-Project/Currency Converter/script.js
@@ -0,0 +1,24 @@
+const amountInput = document.querySelector('input');
+const dropdown1 = document.getElementById('dropdown1');
+const dropdown2 = document.getElementById('dropdown2');
+const convertBtn = document.querySelector('.convert');
+const result = document.querySelector('.result');
+
+const exchangeRates = {
+    'us': { 'india': 85.91, 'Yuan': 11.71 },
+    'india': { 'us': 1 / 85.91, 'Yuan': 1 / 85.91 * 11.71},
+    'Yuan': { 'us': 1 / 11.71, 'india': 1 / 11.71 * 85.91 }
+};
+
+convertBtn.addEventListener('click', () => {
+    const amount = parseFloat(amountInput.value);
+    const fromCurrency = dropdown1.value;
+    const toCurrency = dropdown2.value;
+
+    if (isNaN(amount) || fromCurrency === "Select" || toCurrency === "Select") {
+        alert("Invalid input.");
+        return;
+    }
+
+    result.textContent = `${(amount * exchangeRates[fromCurrency][toCurrency]).toFixed(2)} ${toCurrency.toUpperCase()}`;
+});
